e53057cd9ce80436e784f40d1a70193517b8ff09
GitKodland <125306926+GitKodland@users.noreply.github.com>
2023-07-03 16:56:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# Import
+# Importar
 from flask import Flask, render_template,request, redirect
-# Importing the database library
+# Importando la biblioteca de bases de datos
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-# Connecting SQLite
+# Conectando SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Creating a DB
+# Creación de una base de datos
 db = SQLAlchemy(app )
 
-#Assignment #1. Create a DB table
+#Asignación #1. Crear una tabla de base de datos
 
 
 
@@ -23,32 +23,32 @@ db = SQLAlchemy(app )
 
 
 
-# Running the page with content
+# Ejecutar la página con contenido
 @app.route('/')
 def index():
-    # Displaying the DB objects
-    # Assignment #2. Display the objects from the DB in index.html
+    # Visualización de los objetos de la DB
+    # Asignación #2. Mostrar los objetos de la DB en index.html
     
 
     return render_template('index.html',
-                           #cards = cards
+                           #tarjetas = tarjetas
 
                            )
 
-# Running the page with the card
+# Ejecutar la página con la tarjeta
 @app.route('/card/<int:id>')
 def card(id):
-    # Assignment #2. Display the right card by its id
+    # Asignación #2. Mostrar la tarjeta correcta por su id
     
 
     return render_template('card.html', card=card)
 
-# Running the page and creating the card
+# Ejecutar la página y crear la tarjeta
 @app.route('/create')
 def create():
     return render_template('create_card.html')
 
-# The card form
+# El formulario de la tarjeta
 @app.route('/form_create', methods=['GET','POST'])
 def form_create():
     if request.method == 'POST':
@@ -56,7 +56,7 @@ def form_create():
         subtitle =  request.form['subtitle']
         text =  request.form['text']
 
-        # Assignment #2. Create a way to store data in the DB
+        # Asignación #2. Crear una forma de almacenar datos en la DB
         
 
 
